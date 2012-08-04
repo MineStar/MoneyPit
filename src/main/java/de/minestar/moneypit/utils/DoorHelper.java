669d2e2566f49c374e3694b140b5edd717f21221
GeMoschen <x-chris-x@arcor.de>
2012-08-04 13:13:32 +0200

    - implemented methods for opening DoubleDoors with one click

--- src/main/java/de/minestar/moneypit/utils/DoorHelper.java
+++ src/main/java/de/minestar/moneypit/utils/DoorHelper.java
@@ -53,4 +53,139 @@ public class DoorHelper {
             }
         }
     }
+
+    public static boolean validateDoorBlocks(Block[] firstDoor, Block[] secondDoor) {
+        if (firstDoor[0].getData() == secondDoor[0].getData() && (firstDoor[1].getData() == secondDoor[1].getData() + 1 || firstDoor[1].getData() == secondDoor[1].getData() - 1))
+            return true;
+        return false;
+    }
+
+    public static Block getLowerDoorPart(Block block) {
+        if (block.getData() < 8) {
+            return block;
+        } else {
+            return block.getRelative(BlockFace.DOWN);
+        }
+    }
+
+    public static Block getUpperDoorPart(Block block) {
+        if (block.getData() < 8) {
+            return block.getRelative(BlockFace.UP);
+        } else {
+            return block;
+        }
+    }
+
+    public static void toggleSecondDoor(Block block) {
+        Block lower = DoorHelper.getLowerDoorPart(block);
+
+        if (lower.getData() < 4) {
+            DoorHelper.openSecondDoor(lower);
+        } else {
+            DoorHelper.closeSecondDoor(lower);
+        }
+    }
+
+    public static void openSecondDoor(Block block) {
+        Block[] secondDoor = DoorHelper.getOppositeDoorBlocks(block);
+        if (secondDoor[0] != null && secondDoor[1] != null) {
+            if (DoorHelper.validateDoorBlocks(getDoorBlocks(block), secondDoor)) {
+                secondDoor[0].setData((byte) (secondDoor[0].getData() + 4), false);
+                secondDoor[1].setData((byte) (secondDoor[1].getData()), false);
+            }
+        }
+    }
+
+    public static void closeSecondDoor(Block block) {
+        Block[] secondDoor = DoorHelper.getOppositeDoorBlocks(block);
+        if (secondDoor[0] != null && secondDoor[1] != null) {
+            if (DoorHelper.validateDoorBlocks(getDoorBlocks(block), secondDoor)) {
+                secondDoor[0].setData((byte) (secondDoor[0].getData() - 4), false);
+                secondDoor[1].setData((byte) (secondDoor[1].getData()), false);
+            }
+        }
+    }
+
+    public static Block[] getOppositeDoorBlocks(Block block) {
+        Block[] list = new Block[2];
+        list[0] = DoorHelper.getOppositeLowerDoorPart(block);
+        list[1] = DoorHelper.getOppositeUpperDoorPart(block);
+        return list;
+    }
+
+    public static Block[] getDoorBlocks(Block block) {
+        Block[] list = new Block[2];
+        list[0] = DoorHelper.getLowerDoorPart(block);
+        list[1] = DoorHelper.getUpperDoorPart(block);
+        return list;
+    }
+
+    public static Block getOppositeUpperDoorPart(Block block) {
+        Block selfLower = DoorHelper.getLowerDoorPart(block);
+        Block selfUpper = DoorHelper.getUpperDoorPart(block);
+        final byte dataLower = selfLower.getData();
+        final byte dataUpper = selfUpper.getData();
+        switch (dataLower) {
+            case 0 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(0, 0, +1);
+                else
+                    return selfUpper.getRelative(0, 0, -1);
+            }
+            case 1 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(-1, 0, 0);
+                else
+                    return selfUpper.getRelative(+1, 0, 0);
+            }
+            case 2 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(0, 0, -1);
+                else
+                    return selfUpper.getRelative(0, 0, +1);
+            }
+            case 3 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(+1, 0, 0);
+                else
+                    return selfUpper.getRelative(-1, 0, 0);
+            }
+            case 4 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(0, 0, +1);
+                else
+                    return selfUpper.getRelative(0, 0, -1);
+            }
+            case 5 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(-1, 0, 0);
+                else
+                    return selfUpper.getRelative(+1, 0, 0);
+            }
+            case 6 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(0, 0, -1);
+                else
+                    return selfUpper.getRelative(0, 0, +1);
+            }
+            case 7 : {
+                if (dataUpper == 8)
+                    return selfUpper.getRelative(+1, 0, 0);
+                else
+                    return selfUpper.getRelative(-1, 0, 0);
+            }
+            default : {
+                return null;
+            }
+        }
+    }
+
+    public static Block getOppositeLowerDoorPart(Block block) {
+        Block upper = DoorHelper.getOppositeUpperDoorPart(block);
+        if (upper == null) {
+            return null;
+        } else {
+            return upper.getRelative(BlockFace.DOWN);
+        }
+    }
 }
