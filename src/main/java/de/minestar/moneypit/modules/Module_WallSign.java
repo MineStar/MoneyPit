9c3a00e7ff5215f84061b998f0c85864998abaec
GeMoschen <x-chris-x@arcor.de>
2012-09-03 13:20:39 +0300

    changed returntype for wallSign.addProtection

--- src/main/java/de/minestar/moneypit/modules/Module_WallSign.java
+++ src/main/java/de/minestar/moneypit/modules/Module_WallSign.java
@@ -24,7 +24,7 @@ public class Module_WallSign extends Module {
     }
 
     @Override
-    public void addProtection(Protection protection, byte subData) {
+    public boolean addProtection(Protection protection, byte subData) {
         // get the anchor
         BlockVector anchor = SignHelper.getAnchor(protection.getVector(), subData);
 
@@ -36,6 +36,6 @@ public class Module_WallSign extends Module {
         PhysicsHelper.protectNonSolidBlocks(protection, anchor);
 
         // register the protection
-        getProtectionManager().addProtection(protection);
+        return getProtectionManager().addProtection(protection);
     }
 }
