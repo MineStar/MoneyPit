60cbfe74c1ae5b899510cc3a4a9d5c2e683bd5b6
GeMoschen <x-chris-x@arcor.de>
2015-06-30 09:26:53 +0200

    + added ArmorStand

---
+++ src/main/java/de/minestar/moneypit/modules/Module_ArmorStand.java
@@ -0,0 +1,39 @@
+package de.minestar.moneypit.modules;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.bukkit.gemo.patchworking.IProtection;
+
+import de.minestar.moneypit.MoneyPitCore;
+import de.minestar.moneypit.data.protection.Protection;
+import de.minestar.moneypit.manager.ModuleManager;
+import de.minestar.moneypit.utils.PhysicsHelper;
+
+public class Module_ArmorStand extends Module {
+
+    private final String NAME = "armorstand";
+
+    public Module_ArmorStand(YamlConfiguration ymlFile) {
+        this.writeDefaultConfig(NAME, ymlFile);
+    }
+
+    public Module_ArmorStand(ModuleManager moduleManager, YamlConfiguration ymlFile) {
+        super();
+        this.init(moduleManager, ymlFile, Material.ARMOR_STAND.getId(), NAME);
+    }
+
+    @Override
+    public boolean addProtection(IProtection protection, byte subData, boolean saveToDatabase) {
+        // protect the block below
+        IProtection subProtection = new Protection(protection.getVector().getRelative(0, -1, 0), protection);
+        protection.addSubProtection(subProtection);
+        MoneyPitCore.databaseManager.createSubProtection(subProtection, saveToDatabase);
+
+        // fetch non-solid-blocks
+        PhysicsHelper.protectNonSolidBlocks(protection, subProtection.getVector(), saveToDatabase);
+
+        // register the protection
+        return getProtectionManager().addProtection(protection);
+    }
+}
